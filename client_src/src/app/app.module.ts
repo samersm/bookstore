43b14411caef6881e33aaf741f6c029b42710722
Samer Zagzoog <mrsamer.dev@gmail.com>
2018-05-01 16:47:42 +0300

    Added angular pages, models, routes and services

--- client_src/src/app/app.module.ts
+++ client_src/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpClientModule }    from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { BooksComponent } from './pages/books/books.component';
+import { AddBookComponent } from './pages/books/add-book/add-book.component';
+import { EditBookComponent } from './pages/books/edit-book/edit-book.component';
+import { BookDetailsComponent } from './pages/books/book-details/book-details.component';
+import { AppRoutingModule } from './config/app-routing.module';
+import { BookService } from './services/book.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    BooksComponent,
+    AddBookComponent,
+    EditBookComponent,
+    BookDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client_src/src/app/config/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from '../pages/dashboard/dashboard.component';
+import { AddBookComponent } from '../pages/books/add-book/add-book.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'add-book', component: AddBookComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
